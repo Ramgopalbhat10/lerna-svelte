e235709acdf2bfca1fbe70db0e5a99859244cb17
Subbu <ramgopalbhat10@gmail.com>
2022-04-18 12:12:51 +0530

    css changes

--- packages/mdx-blog/src/lib/components/common/Header.svelte
+++ packages/mdx-blog/src/lib/components/common/Header.svelte
@@ -25,6 +25,14 @@
   <Navbar {menus} {name} {alt} {logo} {textsize} 
     spanClass="mx-2 my-0.5 text-gray-300 text-xl" 
     navClass="active:text-gray-800"
-    liLinkClass="hover:border-b-underline hover:border-b-2 hover:text-highlight text-gray-300 text-base m-1.5"
+    liLinkClass="hover:border-b-underline hover:border-b-2 hover:text-highlight text-gray-300 text-base m-1.5 nav-links"
   />
 </div>
+
+<!-- <style lang="scss">
+  .nav-links {
+    ul > li {
+      color: #22d3ee !important;
+    }
+  }
+</style> -->
--- packages/mdx-blog/src/routes/contact/index.svelte
+++ packages/mdx-blog/src/routes/contact/index.svelte
@@ -4,7 +4,7 @@
 
 <h1>Get in touch</h1>
 <ul>
-  <li><a href="mailto@ramgopalbhat10@gmail.com">Email me!</a></li>
-  <li><a href="https://twitter.com/Batmansubbu">DM me on twitter</a></li>
-  <li><a href="https://www.linkedin.com/in/ramgopal-bhat/">Connect with on LinkedIn</a></li>
+  <li><a href="mailto@ramgopalbhat10@gmail.com">Email</a></li>
+  <li><a href="https://twitter.com/Batmansubbu">Twitter</a></li>
+  <li><a href="https://www.linkedin.com/in/ramgopal-bhat/">LinkedIn</a></li>
 </ul>
